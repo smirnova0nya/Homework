3bdcb1c6c2777a17e53178ebc6d0e8b3a0c95625
Anna Smirnova <annaaleksevnaa@gmail.com>
2022-10-20 00:38:27 +0300

    Домашние задания

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.WriteLine("Введите первое число > ") ;
+int a = Convert.ToInt32(System.Console.ReadLine()) ;
+
+System.Console.WriteLine("Введите второе число > ") ;
+int b = Convert.ToInt32(System.Console.ReadLine()) ;
+
+if(a < b) 
+{
+ System.Console.WriteLine(b);
+}
+else
+{
+ System.Console.WriteLine(a);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.WriteLine("Введите число > ") ;
+int a = Convert.ToInt32(System.Console.ReadLine()) ;
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Even");
+}
+else
+{
+     System.Console.WriteLine("Odd");
+}
